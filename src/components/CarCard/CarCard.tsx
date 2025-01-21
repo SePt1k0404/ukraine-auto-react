74e2d21835f657764fe8c741c4df791aefd27614
SePt1k0404 <d.yra3004@gmail.com>
2025-01-21 21:19:19 +0200

    Rewriting homepage, footer, carCard on Tailwind

--- src/components/CarCard/CarCard.tsx
+++ src/components/CarCard/CarCard.tsx
@@ -1,4 +1,3 @@
-import styles from './CarCard.module.css';
 import {
   AiFillHeart,
   AiOutlineCalendar,
@@ -8,32 +7,38 @@ import {
 
 export const CarCard = () => {
   return (
-    <li className={styles['car-card']}>
+    <li
+      className='group bg-[linear-gradient(145deg,#ffffff,#f1f1f1)]
+     border-solid border-[1px] border-[#ddd] flex flex-col items-start
+      overflow-hidden rounded-xl shadow-lg transition ease hover:-translate-y-2 hover:scale-[1.01] hover:shadow-xl duration-300'
+    >
       <img
         src='https://autodnk.com/wp-content/uploads/2024/08/911-gt3-rs-1.webp'
         alt='Car Model'
-        className={styles['car-card__image']}
+        className='w-full h-48 object-cover transition ease hover:brightness-90 duration-300'
       />
-      <div className={styles['car-card__info']}>
-        <h2 className={styles['car-card__title']}>Car Model</h2>
-        <p className={styles['car-card__year']}>
-          <AiOutlineCalendar className={styles['car-card__calendar-icon']} />{' '}
-          Year: 2025
+      <div className='w-full p-5'>
+        <h2 className='text-2xl font-bold my-2 mx-0 text-secondary-text transition-colors ease hover:text-main-color duration-300'>
+          Car Model
+        </h2>
+        <p className='flex items-center gap-2 text-base text-gray-500 my-1 mx-0'>
+          <AiOutlineCalendar className='w-5 h-5' /> Year: 2025
         </p>
-        <p className={styles['car-card__features']}>
-          <AiOutlineTool className={styles['car-card__key-icon']} /> Key
-          features: Fast, Reliable
+        <p className='flex items-center gap-2 text-base text-gray-500 my-1 mx-0'>
+          <AiOutlineTool className='w-5 h-5' /> Key features: Fast, Reliable
         </p>
-        <p className={styles['car-card__price']}>
-          <AiOutlineDollar className={styles['car-card__price-icon']} /> Price:
-          $100,000
+        <p className='flex items-center gap-2 text-base text-green-500 font-bold my-1 mx-0'>
+          <AiOutlineDollar className='w-5 h-5' /> Price: $100,000
         </p>
-        <div className={styles['car-card__actions']}>
-          <button className={styles['car-card__like-btn']}>
-            <AiFillHeart className={styles['car-card__like-icon']} /> Like
+        <div className='flex justify-between items-center gap-4 mt-5 w-full'>
+          <button
+            className='flex items-center justify-center gap-3 bg-[linear-gradient(145deg,#3498db,#2980b9)] cursor-pointer font-bold
+          text-base border-none rounded-lg py-2 px-4 text-white shadow-md transition-all ease hover:scale-105 hover:bg-[linear-gradient(145deg,#2980b9,#3498db)] active:scale-95 group-hover:shadow-[0_6px_12px_rgba(52,152,219,0.4)] duration-300'
+          >
+            <AiFillHeart className='w-5 h-5' /> Like
           </button>
-          <span className={styles['car-card__likes']}>
-            <AiFillHeart className={styles['car-card__likes-icon']} /> 10 likes
+          <span className='flex items-center gap-2 text-[#34495e] text-sm'>
+            <AiFillHeart className='w-5 h-5 text-red-600' /> 10 likes
           </span>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Arial', 'sans-serif'],
+      },
+      colors: {
+        'background-light': 'rgb(179, 187, 187)',
+        'main-color': '#3498db',
+        'secondary-color': '#2980b9',
+        'secondary-text': '#2c3e50',
+        'footer-dark': '#34495e',
+        'footer-light': '#2c3e50',
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+        fadeInUp: 'fadeInUp 0.5s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(-20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(40px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
