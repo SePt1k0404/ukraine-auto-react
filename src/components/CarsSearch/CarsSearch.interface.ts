4dd0ad23b277114faa665d57a76339aef1052fbe
SePt1k0404 <d.yra3004@gmail.com>
2025-02-07 16:00:26 +0200

    Done geo-location logic

--- src/components/CarsSearch/CarsSearch.interface.ts
+++ src/components/CarsSearch/CarsSearch.interface.ts
@@ -2,3 +2,12 @@ export interface ICarsSearchProps {
   className: string;
   carsListType: 'home' | 'favorite';
 }
+
+export interface ICarsSearchInitialValues {
+  model: string;
+  year: string;
+  price: string;
+  geoLocationCars: boolean;
+  latitude: null | number;
+  longitude: null | number;
+}
